6484e079cc7e770f5233546f9a801bba8266d90c
hong19860320 <9973393+hong19860320@users.noreply.github.com>
2022-04-06 01:11:10 +0000

    Keep sdk and workspace directory

--- tools/compile_scripts/create_and_publish_package.sh
+++ tools/compile_scripts/create_and_publish_package.sh
@@ -3,6 +3,6 @@ set -e
 
 source settings.sh
 
-cd $ROOT_DIR/.. && tar -czvf PaddleLite-generic-demo.tar.gz --exclude=".git" --exclude="*.nb" --exclude="log.txt" --exclude="Makefile" --exclude="CMakeFiles" --exclude="assets/models/*" --exclude="tools/compile_scripts/sdk" --exclude="tools/compile_scripts/workspace" --exclude="tools/compile_scripts/settings.sh" PaddleLite-generic-demo
+cd $ROOT_DIR/.. && tar -czvf PaddleLite-generic-demo.tar.gz --exclude=".git" --exclude="*.nb" --exclude="log.txt" --exclude="Makefile" --exclude="CMakeFiles" --exclude="assets/models/*" --exclude="tools/compile_scripts/sdk/*" --exclude="tools/compile_scripts/workspace/*" --exclude="tools/compile_scripts/settings.sh" PaddleLite-generic-demo
 
 echo "all done."
